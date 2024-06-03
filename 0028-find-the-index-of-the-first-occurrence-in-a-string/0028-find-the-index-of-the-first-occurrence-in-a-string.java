869570bc00fbaea1d9c64c992f319b89084a5312
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-06-03 15:40:44 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (78.81%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack.indexOf(needle)!=-1) {
+			 return haystack.indexOf(needle);
+		} else {
+            return -1;
+		}
+    }
+}
