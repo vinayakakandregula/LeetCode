83d46956c86d7eea277f1e1731e283550957d2c6
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-06-05 11:36:44 +0530

    Time: 1 ms (21.41%), Space: 41.8 MB (27.70%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void sortColors(int[] a) {
+ for(int i=0;i<a.length-1;i++) {
+			int min=i;
+			for(int j=i+1;j<a.length;j++) {
+				if(a[min]>a[j]){
+					min =j;
+				}
+			}
+			int temp = a[min];
+			a[min] = a[i];
+			a[i] = temp;
+		}   
+    }
+}
