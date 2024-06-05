8bacbb321ebf73f566f449f74eb4f2d49a2afd52
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-06-05 13:30:28 +0530

    Time: 2 ms (99.30%), Space: 42 MB (72.81%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+       StringBuilder result = new StringBuilder();
+    int openCount = 0;
+
+    for (char c : s.toCharArray()) {
+        if (c == '(') {
+            if (openCount > 0) {
+                result.append(c);
+            }
+            openCount++;
+        } else if (c == ')') {
+            openCount--;
+            if (openCount > 0) {
+                result.append(c);
+            }
+        }
+    }
+
+    return result.toString();
+		
+    }
+}
