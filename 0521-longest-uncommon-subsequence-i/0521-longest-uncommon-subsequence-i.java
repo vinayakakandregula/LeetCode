bf6d2d1562a3c5b352b91b4acf5bec65273554f3
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-06-05 12:32:09 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (85.24%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if(a.equals(b)){
+            return -1;
+        }
+        if(a.length()>b.length()){
+            return a.length();
+        } else{
+            return b.length();
+        }
+    }
+}
