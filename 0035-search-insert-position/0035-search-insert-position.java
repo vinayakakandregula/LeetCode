deeddd95580a2dc592f4ca6285e0fa1816b81195
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-07-04 09:44:25 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (96.11%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       int start = 0;
+        int end = nums.length-1;
+        int mid = (start+end)/2;
+        while(start<=end){
+            mid = (start+end)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                end  = mid-1;
+            } else{
+                start = mid+1;
+            }
+            if(end < start){
+                return start;
+            }
+        }
+        return 0;
+    }
+}
