ad1241b6b5c6f769f58c91dec9bf3a20ccc433d9
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-07-09 12:51:22 +0530

    Time: 7 ms (41.98%), Space: 46.6 MB (74.94%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -4,18 +4,18 @@ class Solution {
 		for (int i = 0; i < nums.length; i++) {
 			res[i] = nums[i] * nums[i];
 		}
-//		Arrays.sort(res);
-		for (int i = 0; i < res.length; i++) {
-			int min = i;
-			for (int j = i + 1; j < res.length; j++) {
-				if (res[j] < res[min]) {
-					min = j;
-				}
-			}
-			int temp = res[min];
-			res[min] = res[i];
-			res[i] = temp;
-		}
+		Arrays.sort(res);
+		// for (int i = 0; i < res.length; i++) {
+		// 	int min = i;
+		// 	for (int j = i + 1; j < res.length; j++) {
+		// 		if (res[j] < res[min]) {
+		// 			min = j;
+		// 		}
+		// 	}
+		// 	int temp = res[min];
+		// 	res[min] = res[i];
+		// 	res[i] = temp;
+		// }
         return res;
     }
 }
