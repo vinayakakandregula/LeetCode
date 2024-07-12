975ae9f3789bd4ce5203128f6551ceea179565f8
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-07-12 10:40:53 +0530

    Time: 1 ms (66.07%), Space: 40.5 MB (53.13%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = check(n);
+        }
+        return n == 1;
+    }
+
+    public static int check(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int rem = n % 10;
+            sum += rem * rem;
+            n /= 10;
+        }
+        return sum;
+    }
+}
