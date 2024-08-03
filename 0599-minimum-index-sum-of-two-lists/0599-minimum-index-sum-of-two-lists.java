6129adb95ee254e3f8d8c821735c74631b895da4
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-08-03 23:10:30 +0530

    Time: 110 ms (13.59%), Space: 46 MB (7.41%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
@@ -1,26 +1,20 @@
 class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < list1.length; i++) {
-            map.put(list1[i], i);
-        }
-        
-        List<String> result = new ArrayList<>();
         int minSum = Integer.MAX_VALUE;
-        
-        for (int j = 0; j < list2.length; j++) {
-            if (map.containsKey(list2[j])) {
-                int sum = j + map.get(list2[j]);
-                if (sum < minSum) {
-                    minSum = sum;
+		List<String> result = new ArrayList<String>();
+		for (int i = 0; i < list1.length; i++) {
+			for (int j = 0; j < list2.length; j++) {
+				if (list1[i].equals(list2[j])) {
+					if ((i+j) < minSum) {
+                    minSum = i+j;
                     result.clear();  
                     result.add(list2[j]);
-                } else if (sum == minSum) {
+                } else if ((i+j) == minSum) {
                     result.add(list2[j]);  
                 }
-            }
-        }
-        
-        return result.toArray(new String[result.size()]);
+				}
+			}
+		}
+	return result.toArray(new String[result.size()]);
     }
 }
