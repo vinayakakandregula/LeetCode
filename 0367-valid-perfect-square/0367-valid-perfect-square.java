e2a91cc96a4476df97ee993e2b57a9e796deda91
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-09-07 18:49:42 +0530

    Time: 1 ms (20.56%), Space: 40.1 MB (85.49%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int i=1;
+        while(num>0){
+            num -= i;      
+            i +=2;         
+            if(num == 0) return true;
+        }
+        return false;
+    }
+}
