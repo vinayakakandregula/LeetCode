dc1d3fdab941e7b7ebda1f9312b655b140419fc4
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-09-10 11:38:09 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (99.09%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int n = 0;
+        if(a>b){
+            n = b;
+        } else{
+            n = a;
+        }
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            if(a%i==0 && b%i==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
