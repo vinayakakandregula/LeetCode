b72210bd7c535aae6c3df7bc3e418a2b154e0575
Kandregula Vinayaka <128498589+vinayakakandregula@users.noreply.github.com>
2024-09-17 19:34:56 +0530

    Time: 3 ms (5.52%), Space: 40.7 MB (94.71%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int stone : stones) {
+            maxHeap.add(stone);
+        }
+        
+        while (maxHeap.size() > 1) {
+            int stone1 = maxHeap.poll();  
+            int stone2 = maxHeap.poll();  
+            
+            if (stone1 != stone2) {
+                maxHeap.add(stone1 - stone2);
+            }
+        }
+        
+        return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+    }
+}
